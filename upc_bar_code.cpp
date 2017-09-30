e637125140880f37a25b680d4d846ecb0ac8721e
Buster Schrader <bstrdash@gmail.com>
2017-09-29 23:55:07 -0500

    Introduced new bar code class.

---
+++ upc_bar_code.cpp
@@ -0,0 +1,16 @@
+#include "upc_bar_code.hpp"
+
+upc_bar_code::upc_bar_code()
+{
+	
+}
+
+upc_bar_code::~upc_bar_code()
+{
+	
+}
+
+upc_bar_code::write()
+{
+	
+}
---
+++ upc_bar_code.hpp
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////
+// UPC-A Bar Code Class
+//
+// 4/6/17 - B.D.S.
+//
+// Purpose: Reads and writes UPC-A bar codes.
+////////////////////////////////////////////////////////////////
+
+#ifndef _UPC_BAR_CODE_HPP
+#define _UPC_BAR_CODE_HPP
+
+//#include "windows_bitmap.hpp"
+
+class upc_bar_code
+{
+	private:
+		short module_set[10];
+		long bar_code[3];
+	public:
+		upc_bar_code();
+		~upc_bar_code();
+		write();
+};
+
+#endif
+
